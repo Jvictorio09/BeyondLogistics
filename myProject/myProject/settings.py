228af2f085dd915bdaf893498adcdbb16399a12f
Julia <juliavictorio16@Gmail.com>
2024-10-23 19:34:34 +0800

    updated layout

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-=b1xxdpfm*qbbg1&^%*v#@3jnvy+hg--6zis^knoetfdzha688
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'beyondlogistics-production.up.railway.app',
+    '127.0.0.1',  # If you also want to allow local development
+    'localhost',
+]
+
 
 
 # Application definition
@@ -132,3 +137,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.your-email-provider.com'  # e.g., smtp.gmail.com
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
